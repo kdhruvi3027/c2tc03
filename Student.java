3974baf5c8433db1da3b08bcfa8085c58508da36
Dhruvi Koradiya <134957095+kdhruvi3027@users.noreply.github.com>
2023-06-02 11:51:20 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,10 +1,12 @@
+//class Student1.java
 //4)  Write a program in java contains two class Student(main class) and Student1.
 //Student1 contains the members like name, id, age, mobileno and member function getdata() 
 //to get the data and display() to display the data , create a object of class Student1 and 
 //use the member function getdata() and display() to enter the data and display the data respectively.
 
 public class Student {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Student1 student1 = new Student1();
 
         System.out.println("Enter student details:");
